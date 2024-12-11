d0ed50beddb805e384a7bfcdd5871f7eda7274a1
Dmitry Sedov <sedov.dmitry1@gmail.com>
2024-12-11 19:52:23 +0300

    Modal to functional, remove local state, unmountOnExit for CSSTransition

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,50 +1,35 @@
-import React, { Component, ReactNode } from "react";
+import React, { FC } from "react";
 import { CSSTransition } from "react-transition-group";
-
 import { Close } from "../Close";
-
 import "./style.css";
 
-interface State {
-  opened: boolean;
-}
-
-interface Props {
+interface ModalProps {
   opened: boolean;
   onClose: () => void;
+  children: React.ReactNode;
 }
 
-export class Modal extends Component<Props, State> {
-  public readonly state: State = {
-    opened: false,
-  };
-
-  public static getDerivedStateFromProps(props: Props, state: State): State {
-    return { opened: props.opened };
-  }
-
-  public render(): ReactNode {
-    return (
-      <CSSTransition
-        classNames={{
-          enter: "modal_enter",
-          enterActive: "modal_enter-active",
-          enterDone: "modal_enter-done",
-          exit: "modal_exit",
-          exitActive: "modal_exit-active",
-          exitDone: "modal_exit-done",
-        }}
-        in={this.state.opened}
-        timeout={300}
-      >
-        <div className="modal">
-          <div className="modal--content">
-            <Close onClick={this.props.onClose} />
-
-            {this.props.children}
-          </div>
+export const Modal: FC<ModalProps> = ({ opened, onClose, children }) => {
+  return (
+    <CSSTransition
+      classNames={{
+        enter: "modal_enter",
+        enterActive: "modal_enter-active",
+        enterDone: "modal_enter-done",
+        exit: "modal_exit",
+        exitActive: "modal_exit-active",
+        exitDone: "modal_exit-done",
+      }}
+      in={opened}
+      timeout={300}
+      unmountOnExit
+    >
+      <div className="modal" role="dialog" aria-modal="true">
+        <div className="modal--content">
+          <Close onClick={onClose} />
+          {children}
         </div>
-      </CSSTransition>
-    );
-  }
-}
+      </div>
+    </CSSTransition>
+  );
+};
